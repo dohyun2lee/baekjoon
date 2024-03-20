de70fda2810e13ea74783bb454aec8fe6bbf7bc0
unknown <eric8800@naver.com>
2024-03-20 20:20:45 +0900

    20240320

---
+++ 5014.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var F, S, G, U, D, cnt int
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(reader, &F, &S, &G, &U, &D)
+
+}
