e73b63c4fbb0f3bdefa5269d69bf2d4417309bf0
unknown <eric8800@naver.com>
2024-04-08 18:23:50 +0900

    20240408

---
+++ 2167.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var N, M, K, a, b, c, d, ans int
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	fmt.Fscanln(reader, &N, &M)
+	var board [][]int = make([][]int, N)
+
+	for i := 0; i < N; i++ {
+		board[i] = make([]int, M)
+		for j := 0; j < M; j++ {
+			if j != M-1 {
+				fmt.Fscan(reader, &board[i][j])
+			} else {
+				fmt.Fscanln(reader, &board[i][j])
+			}
+		}
+	}
+
+	fmt.Fscanln(reader, &K)
+
+	for i := 0; i < K; i++ {
+		ans = 0
+		fmt.Fscanln(reader, &a, &b, &c, &d)
+
+		for j:=a-1;j<c;j++ {
+			for k:=b;k<d;k++ {
+				ans += board[j][k]
+			}
+		}
+
+		fmt.Fprintln(writer, ans)
+	}
+}
