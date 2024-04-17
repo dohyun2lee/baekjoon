6f6401c8aeeb8583bf0f029eb6a24f9997abb95b
unknown <eric8800@naver.com>
2024-04-17 18:47:57 +0900

    20240417

---
+++ 3986.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	//"container/list"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var N, cnt int
+	var S string
+	var goods []string
+	//goods := list.New()
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(reader, &N)
+
+	for i:=0;i<N;i++ {
+		fmt.Fscanln(reader, &S)
+
+		for j:=0;j<len(S);j++ {
+			if j != 0 {
+				if string(S[j]) == goods[len(goods)-1] {
+					goods = goods[:len(goods)-1]
+				} else {
+					goods = append(goods, string(S[j]))
+				}
+			} else  {
+				goods = append(goods, string(S[j]))
+			}
+		}
+
+		if len(goods) == 0 {
+			cnt++
+		}
+	}
+
+	fmt.Println(cnt)
+}
