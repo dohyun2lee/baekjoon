851afc9b5b548785da3a8b8523bf0a34484ead2d
unknown <eric8800@naver.com>
2024-04-22 18:48:15 +0900

    20240422

--- 5355.go
+++ 5355.go
@@ -10,7 +10,6 @@ import (
 
 func main() {
 	var T int
-	var N float64
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
@@ -22,20 +21,17 @@ func main() {
 		scanner.Scan()
 		x := strings.Split(scanner.Text(), " ")
 
-		fmt.Println(x)
+		N, _ := strconv.ParseFloat(x[0], 32)
+		for j := 1; j < len(x); j++ {
 
-		for j := 0; j < len(x); j++ {
-			if j == 0 {
-				N, _ = strconv.ParseFloat(x[j], 32)
-			} else {
-				if x[j] == "@" {
-					N *= 3
-				} else if x[j] == "%" {
-					N += 5
-				} else if x[j] == "#" {
-					N -= 7
-				}
+			if x[j] == "@" {
+				N *= 3
+			} else if x[j] == "%" {
+				N += 5.0
+			} else if x[j] == "#" {
+				N -= 7.0
 			}
+
 		}
 
 		fmt.Fprintf(writer, "%.2f\n", N)
