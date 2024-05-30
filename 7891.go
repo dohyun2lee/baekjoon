a248ad0bb17f993379c0949fc87ba0100d236bd2
unknown <eric8800@naver.com>
2024-05-30 16:24:51 +0900

    20240530

---
+++ 7891.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+)
+
+func main() {
+	var A, B big.Int
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(reader, &A)
+	fmt.Fscanln(reader, &B)
+
+	a := new(big.Int)
+	fmt.Println(a.Add(&A, &B))
+}
